bc5862748a62830269fb6acf5f4679d3b7563799
Wagner Bigal <wagnerbigal@gmail.com>
2016-04-16 20:10:58 -0300

    Alterando para receber o XML para parsear

--- app/parsers/speaker_line_parser.rb
+++ app/parsers/speaker_line_parser.rb
@@ -1,9 +1,13 @@
+require 'xmlsimple'
+
 class SpeakerLineParser
-  def to_counter doc
+  def to_speaker_line_counter doc
     speaker_line_counter = SpeakerLineCounter.new
     return speaker_line_counter if (doc.nil? || doc.empty?)
 
-    doc["ACT"].each do |ect|
+    doc_hash = XmlSimple.xml_in(doc)
+
+    doc_hash["ACT"].each do |ect|
       ect["SCENE"].each do |scene|
         scene["SPEECH"].each do |speech|
           speaker_line_counter.add(speech["SPEAKER"].first, speech["LINE"].length)
--- spec/parsers/speaker_line_parser_spec.rb
+++ spec/parsers/speaker_line_parser_spec.rb
@@ -4,18 +4,17 @@ require 'xmlsimple'
 describe SpeakerLineParser do
   let(:speaker_line_parser) { SpeakerLineParser.new }
 
-  context '#to_counter' do
+  context '#to_speaker_line_counter' do
     it 'parser an empty doc' do
-      result = speaker_line_parser.to_counter({})
+      result = speaker_line_parser.to_speaker_line_counter({})
       expect(result).to be_an_instance_of(SpeakerLineCounter)
       expect(result.length).to be(0)
     end
 
     it 'a lot of lines' do
-      file = File.open('./spec/fixtures/macbeth.xml')
-      data  = XmlSimple.xml_in(file)
+      xml = File.open('./spec/fixtures/macbeth.xml').read
 
-      result = speaker_line_parser.to_counter(data)
+      result = speaker_line_parser.to_speaker_line_counter(xml)
       expect(result).to be_an_instance_of(SpeakerLineCounter)
       expect(result.length).to be(6)
       expect(result.get('DUNCAN')).to be(12)
