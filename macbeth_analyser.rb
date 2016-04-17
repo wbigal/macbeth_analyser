86480859aff35235ad364a3fbbd20dd822e26950
Wagner Bigal <wagnerbigal@gmail.com>
2016-04-16 21:33:11 -0300

    Humanizando nomes

--- macbeth_analyser.rb
+++ macbeth_analyser.rb
@@ -2,5 +2,5 @@ Dir[File.dirname(__FILE__) + "/app/**/*.rb"].each { |file| load file }
 
 macbeth_analyser_service = MacbethAnalyserService.new
 macbeth_analyser_service.speakers.each do |key, value|
-  puts "#{value} #{key}"
+  puts "#{value} #{key.humanize}"
 end
